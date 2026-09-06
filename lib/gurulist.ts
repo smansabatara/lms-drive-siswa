a9acb50fefb08cf527328748a5071b23b0f62d35
Smansabatara <ayrasrudham@gmail.com>
2026-09-07 06:17:56 +0700

    Tambah daftar guru baru

--- lib/gurulist.ts
+++ lib/gurulist.ts
@@ -1,12 +1,12 @@
 // Daftar Guru yang terdaftar oleh Admin (Nama & Email)
 export const ALLOWED_GURUS = [
   { name: "I, S.Pd.", email: "budi@sekolah.sch.id" },
-  { name: "Dian Agustin, S.Pd.", email: "siti@sekolah.sch.id" },
+  { name: "Dian Agustin, S.Pd.", email: "dianagustin382@guru.sma.belajar.id" },
   { name: "I Wayan Prasidha K, S.Pd", email: "mprazz204@gmail.com" },
   { name: "Dian Febrintina S, S.Pd.", email: "dian.febri1202@gmail.com" },
-  { name: "Putri Yuliana, S.Pd.", email: "siti@sekolah.sch.id" },
+  { name: "Putri Yuliana, S.Pd.", email: "putriyuliana377@gmail.com" },
   { name: "Reti Widia A, S.Pd.", email: "siti@sekolah.sch.id" },
-  { name: "Endang Lismawati, S.Pd.", email: "siti@sekolah.sch.id" }
+  { name: "Endang Lismawati, S.Pd.", email: "endanglismawati92@gmail.com" }
   
     // Tambahkan daftar nama & email guru di sini
 ];
